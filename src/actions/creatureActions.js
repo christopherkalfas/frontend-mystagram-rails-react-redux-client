8a3895343929cd4fafd4f70abf68d9b6807e083f
christopherkalfas <christopherkalfas22@gmail.com>
2020-08-14 13:46:07 -0400

    Part II Getting Heroku Ready

--- src/actions/creatureActions.js
+++ src/actions/creatureActions.js
@@ -1,7 +1,11 @@
+const DEV_URL = "http://localhost:3001"
+const PROD_URL = "https://mystagram-api.herokuapp.com"
+const API_ROOT = process.env.NODE_ENV === "development" ? DEV_URL : PROD_URL
+
 export const fetchCreatures = () => {
     return (dispatch) => {
         dispatch({type: 'LOADING_CREATURES'})
-            return fetch('http://localhost:3001/creatures')
+            return fetch('${API_ROOT}/creatures')
                 .then(response => {
                     return response.json()
                 })
@@ -13,7 +17,7 @@ export const fetchCreatures = () => {
 
 export const addNewCreature = ( creature, history ) => {
     return dispatch => {
-        return fetch('http://localhost:3001/creatures', {
+        return fetch('http://${API_ROOT}/creatures', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -34,7 +38,7 @@ export const addNewCreature = ( creature, history ) => {
 
 export const updateLikes = (creature, actionType) => {
     return (dispatch) => {
-        return fetch(`http://localhost:3001/creatures/${creature.id}`, {
+        return fetch(`http://${API_ROOT}/creatures/${creature.id}`, {
             method: 'PATCH',
             headers: {
                 'Accept': 'application/json',
@@ -55,7 +59,7 @@ export const updateLikes = (creature, actionType) => {
 
 export const deleteCreature = (id) => {
     return(dispatch) => {
-        return fetch(`http://localhost:3001/creatures/${id}`, {
+        return fetch(`http://${API_ROOT}/creatures/${id}`, {
             method: 'DELETE',
             header: {
                 'Accept': 'application/json',
@@ -69,6 +73,79 @@ export const deleteCreature = (id) => {
         })
     }
 }
+
+
+// export const fetchCreatures = () => {
+//     return (dispatch) => {
+//         dispatch({type: 'LOADING_CREATURES'})
+//             return fetch('http://localhost:3001/creatures')
+//                 .then(response => {
+//                     return response.json()
+//                 })
+//                 .then(responseJSON => {
+//                     dispatch({type: 'ADD_CREATURES', creatures: responseJSON })
+//                 })
+//     }
+// }
+
+// export const addNewCreature = ( creature, history ) => {
+//     return dispatch => {
+//         return fetch('http://localhost:3001/creatures', {
+//             method: 'POST',
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ creature })
+//         })
+//         .then(response => response.json())
+//         .then(creature => {
+//             dispatch({ type: 'ADD_NEW_CREATURE', creature })
+//             //making sure my promise is resolved before redirect
+//             //to make sure front end store has updated
+//             history.push('/creatures')
+//             //push new creature into history 'stack'
+//         })   
+//     }
+// }
+
+// export const updateLikes = (creature, actionType) => {
+//     return (dispatch) => {
+//         return fetch(`http://localhost:3001/creatures/${creature.id}`, {
+//             method: 'PATCH',
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 'likes': creature.likes
+//             })
+//         })
+//             .then(response => {
+//                 return response.json()
+//             })
+//             .then(creature => {
+//                 dispatch({type: actionType, creature})
+//             })
+//     }
+// }
+
+// export const deleteCreature = (id) => {
+//     return(dispatch) => {
+//         return fetch(`http://localhost:3001/creatures/${id}`, {
+//             method: 'DELETE',
+//             header: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//         .then(response => response.json())
+//         .then( responseJSON => {
+//             console.log(responseJSON)
+//             dispatch({type: 'DELETE_CREATURE', id: responseJSON})
+//         })
+//     }
+// }
 
 
 
